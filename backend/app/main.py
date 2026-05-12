4a4eb83b65b22e3e1134255a50ed4142667298af
SAN-900 <savanpatle2004@gmail.com>
2026-05-12 12:01:56 +0530

    Cors fix

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,8 +1,17 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from app.schemas.swap_schema import SwapData
 from app.services.risk_engine import calculate_risk
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # for hackathon/demo
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 def read_root():
